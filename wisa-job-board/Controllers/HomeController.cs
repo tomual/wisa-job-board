55e919164131feae91505d05fbf600bec3db7c00
tomual <tomual@users.noreply.github.com>
2018-03-02 22:09:40 -0600

    Show job list in home

--- wisa-job-board/Controllers/HomeController.cs
+++ wisa-job-board/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WisaJobBoard.Models;
 
 namespace WisaJobBoard.Controllers
 {
     public class HomeController : Controller
     {
+        private JobDBContext db = new JobDBContext();
+
         public ActionResult Index()
         {
+            var jobs = from j in db.Jobs
+                       select j;
+
+            ViewBag.Jobs = jobs;
             return View();
         }
 
