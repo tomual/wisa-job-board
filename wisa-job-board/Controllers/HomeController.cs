9c68d3cc0d844ca79d5d0ead8ff9654f873d22a9
tomual <tomual@users.noreply.github.com>
2018-03-04 14:27:00 -0600

    Home page elements and styling

--- wisa-job-board/Controllers/HomeController.cs
+++ wisa-job-board/Controllers/HomeController.cs
@@ -11,12 +11,27 @@ namespace WisaJobBoard.Controllers
     {
         private JobDBContext db = new JobDBContext();
 
-        public ActionResult Index()
+        public ActionResult Index(string location)
         {
+            var LocationList = new List<string>();
+            var LocationQuery = from d in db.Jobs
+                                orderby d.Location
+                                select d.Location;
+            LocationList.AddRange(LocationQuery.Distinct());
+            SelectList locationSelect = new SelectList(LocationList);
+            SelectListItem selListItem = new SelectListItem() { Value = "", Text = "Select One" };
+            ViewBag.locations = locationSelect;
+
             var jobs = from j in db.Jobs
                        select j;
 
             ViewBag.Jobs = jobs;
+
+            if (!String.IsNullOrEmpty(location))
+            {
+                jobs = jobs.Where(x => x.Location == location);
+            }
+
             return View();
         }
 
